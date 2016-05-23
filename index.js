dbd356a783eb2224f9330588426ac334e8a90815
Cesar Augusto Garcia <cesar.garcia@mulesoft.com>
2016-05-23 19:13:16 -0300

    Adding names generator based on siblings

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+var names   = require('./names.json');
 var phrases = require('./phrases.json');
 
 module.exports = {
+  name:   name,
   phrase: phrase
 };
 
 function phrase() {
   return phrases[Math.floor(Math.random() * phrases.length)];
+}
+
+function name() {
+  return names[Math.floor(Math.random() * names.length)];
 }
